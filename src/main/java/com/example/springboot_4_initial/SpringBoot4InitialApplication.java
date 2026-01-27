5998e13a1447e32df46cfa086978993f3ff3f8ea
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-01-26 20:32:52 -0600

    initial commit

---
+++ src/main/java/com/example/springboot_4_initial/SpringBoot4InitialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_4_initial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4InitialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4InitialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_4_initial/SpringBoot4InitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_4_initial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot4InitialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
